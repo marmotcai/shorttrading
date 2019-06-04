62dc681ec2a3219d5bc608d77d910452e1b9d771
marmotcai <marmotcai@163.com>
2019-06-04 21:39:32 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -27,26 +27,6 @@ class BaseStock():
 
     #########################################################################################
 
-    def istringtime(self):
-        date = datetime.datetime.now()
-        day = date.weekday()
-        if (day > 4):
-            return 0
-
-        am_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '9:30', '%Y-%m-%d%H:%M')
-        am_time2 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '11:30', '%Y-%m-%d%H:%M')
-        pm_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '13:00', '%Y-%m-%d%H:%M')
-        pm_time2 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '15:00', '%Y-%m-%d%H:%M')
-
-        if date >= am_time1 and date <= am_time2:
-            return 1
-        if date >= pm_time1 and date <= pm_time2:
-            return 1
-
-        return 0
-
-    #########################################################################################
-
     def judge(self, marketinfos):
 
         def judge_buy(marketinfo):
