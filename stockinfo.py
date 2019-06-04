41bf97c58eb76cbdacadcabf2ba83f3043416c6b
marmotcai <marmotcai@163.com>
2019-06-04 21:40:46 +0800

    Update stockinfo.py

--- stockinfo.py
+++ stockinfo.py
@@ -67,8 +67,28 @@ class statistics: # 当前状态信息
         self.sell_order = {}  # 卖出记录
         self.sell_count = 0  # 卖出次数
 
-    def get_quota(self):
+    def get_quota(self): # 判断资金余量
         return round(self.primecost / self.startinfo.maximum_capital, 2)
+
+    #########################################################################################
+
+    def istringtime(self): # 判断是否交易日
+        date = datetime.datetime.now()
+        day = date.weekday()
+        if (day > 4):
+            return 0
+
+        am_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '9:30', '%Y-%m-%d%H:%M')
+        am_time2 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '11:30', '%Y-%m-%d%H:%M')
+        pm_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '13:00', '%Y-%m-%d%H:%M')
+        pm_time2 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '15:00', '%Y-%m-%d%H:%M')
+
+        if date >= am_time1 and date <= am_time2:
+            return 1
+        if date >= pm_time1 and date <= pm_time2:
+            return 1
+
+        return 0
 
 class calc:  # 计算工具类
 
