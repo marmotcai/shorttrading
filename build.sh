ee8da67e51f7a83823e304ff49e4e1eadc022814
root <root@ip-172-31-46-157.ap-northeast-1.compute.internal>
2019-06-04 14:06:30 +0000

    auto commit 2019-06-04:14:06:30

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 
 docker build --target python-builder -t marmotcai/python-app -f ./Dockerfile .
 
-docker rm -f my-stock
-docker run -p 80:5588 --name my-stock -d marmotcai/python-app
+docker rm -f my-st
+docker run -p 80:5588 --name my-st -d marmotcai/python-app
